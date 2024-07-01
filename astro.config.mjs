95c3b502ca2b826f5b701498592a2ea34b03bf06
Cristian <cristianvillalobos666@gmail.com>
2024-06-30 22:28:27 -0400

    Configure Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/server'; // Importa el adaptador de Vercel
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/server';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel() // Configura el adaptador de Vercel
+  adapter: vercel(),
 });
