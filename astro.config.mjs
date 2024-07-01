5adc0ace7b865729db0edb95e8644786bc07dc8c
Cristian <cristianvillalobos666@gmail.com>
2024-06-30 22:26:09 -0400

    vercel cli integration in proyect server run output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/server'; // Importa el adaptador de Vercel
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output:"server"
+  output: 'server',
+  adapter: vercel() // Configura el adaptador de Vercel
 });
